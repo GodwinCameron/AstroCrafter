d086cdef3f0c9bea36a3265dd6ffae6c555a8893
Godwin Cameron <200109@virtualwindow.co.za>
2024-03-03 19:32:41 +0200

    planet setup

--- mainapp/src/app/app.routes.ts
+++ mainapp/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { PlanetComponent } from './routes/planet/planet.component';
 
 export const routes: Routes = [
     { path: "home", component: HomeComponent },
-    { path: "", redirectTo: "/home", pathMatch: "full" }
+    { path: "planet", component: PlanetComponent},
+    { path: "", redirectTo: "/home", pathMatch: "full" },
 ];
