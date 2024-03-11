7a0d89b55d42323e9cd9652dde311a1340667b8f
GodwinCameron <71267628+GodwinCameron@users.noreply.github.com>
2024-03-11 19:45:35 +0200

    planet animations added

---
+++ mainapp/src/app/components/inventory-crafting/inventory-crafting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-crafting',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-crafting.component.html',
+  styleUrl: './inventory-crafting.component.sass'
+})
+export class InventoryCraftingComponent {
+
+}
--- mainapp/src/app/routes/planet/planet.component.ts
+++ mainapp/src/app/routes/planet/planet.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InventoryItemComponent } from '../../components/inventory-item/inventory-item.component';
+import { MainPanelComponent } from '../../components/main-panel/main-panel.component';
 
 @Component({
   selector: 'app-planet',
   standalone: true,
-  imports: [InventoryItemComponent],
+  imports: [InventoryItemComponent, MainPanelComponent],
   templateUrl: './planet.component.html',
   styleUrl: './planet.component.sass'
 })
