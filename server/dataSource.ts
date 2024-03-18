80d1f30820e6bdfdd3c1b29f9f8ddf52fb72a3e6
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-03-18 03:49:30 +0200

    working on the entity
    datasource added

--- server/dataSource.ts
+++ server/dataSource.ts
@@ -1,19 +1,19 @@
-// import { DataSource } from "typeorm"
+import { DataSource } from "typeorm"
 
-// const AppDataSource = new DataSource(
-//     {
-//         "type": "postgres",
-//         "host": "localhost",
-//         "port": 5432,
-//         "username": "postgres",
-//         "password": "admin",
-//         "database": "astrocrafter_db",
-//         "entities": ["src/entity/**.ts"],
-//         // "logging": true,
-//         "synchronize": false
-//     }
-//   )
-//   export default AppDataSource
+const AppDataSource = new DataSource(
+    {
+        "type": "postgres",
+        "host": "localhost",
+        "port": 5432,
+        "username": "postgres",
+        "password": "admin",
+        "database": "astrocrafter_db",
+        "entities": ["src/entity/**.ts"],
+        // "logging": true,
+        "synchronize": false
+    }
+  )
+  export default AppDataSource
 
   
   
