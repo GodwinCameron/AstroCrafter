f8201024d3d681dd87a8e474c5bfe8106a21bf88
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-03-18 02:50:18 +0200

    Separating the Server/Backend with mainapp
    encounted an error in the mainapp during this process....will solve

---
+++ server/dataSource.ts
@@ -0,0 +1,19 @@
+// import { DataSource } from "typeorm"
+
+// const AppDataSource = new DataSource(
+//     {
+//         "type": "postgres",
+//         "host": "localhost",
+//         "port": 5432,
+//         "username": "postgres",
+//         "password": "admin",
+//         "database": "astrocrafter_db",
+//         "entities": ["src/entity/**.ts"],
+//         // "logging": true,
+//         "synchronize": false
+//     }
+//   )
+//   export default AppDataSource
+
+  
+  
