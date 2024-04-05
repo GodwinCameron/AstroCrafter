a9d747b62c5952aaf6a208c07a00f0113cc1bfb6
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-04-05 17:37:19 +0200

    Update userRoute.ts

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import AppDataSource from "../dataSource";
 import { Recipe } from "../entity/";
-import {}
+import { Ingredients } from "../entity/ingredinets";
+import { Inventory } from "../entity/inventory"
 
 const userRouter = express.Router()
 
 userRouter.use(express.json())
 
 const appDataSource = AppDataSource
+
+userRouter.post("/", async (req, res) => {
+
+    try {
+
+        const {
+            
+        }
+        
+    } catch (error) {
+        console.log("Error occured: " + error)
+        res.status(500).json({message: error})
+    }
+});
+
+//TODO: create a post endpoint, to create our users
+
+//TODO: create a post endpoint, for login
+
+
+export default userRouter;
