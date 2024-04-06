cfab3b1158e7e0395d62eaeb9e24858e611791e8
GodwinCameron <71267628+GodwinCameron@users.noreply.github.com>
2024-04-06 14:45:46 +0200

    changed ui tab icons setup crafting

--- client/src/app/components/inventory-crafting/inventory-crafting.component.ts
+++ client/src/app/components/inventory-crafting/inventory-crafting.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, Input, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { CommonModule } from '@angular/common';
 import { RequiredIngredientCardComponent } from '../required-ingredient-card/required-ingredient-card.component';
@@ -7,7 +14,7 @@ import { ResourceService } from '../../services/resource.service';
 @Component({
   selector: 'app-inventory-crafting',
   standalone: true,
-  imports: [ CommonModule, RequiredIngredientCardComponent ],
+  imports: [CommonModule, RequiredIngredientCardComponent],
   templateUrl: './inventory-crafting.component.html',
   styleUrl: './inventory-crafting.component.sass',
 })
@@ -15,7 +22,8 @@ export class InventoryCraftingComponent {
   @ViewChild('ingredientsElement', { static: false })
   ingredientsElement!: ElementRef;
   @ViewChild('yeildElement', { static: false }) yeildElement!: ElementRef;
-  @ViewChild('craftButtonElement', { static: false }) craftButtonElement!: ElementRef;
+  @ViewChild('craftButtonElement', { static: false })
+  craftButtonElement!: ElementRef;
 
   recipes: any;
   index: number = 0;
@@ -24,10 +32,12 @@ export class InventoryCraftingComponent {
   @Input() currentPlanet: any;
   @Input() wallet: number = 0;
   @Input() inventory: any;
+  @Input() ingredientsMet: Array<boolean> = [false, false, false];
 
-  
-
-  constructor(private service: RecipeService, private resourceService: ResourceService) {}
+  constructor(
+    private service: RecipeService,
+    private resourceService: ResourceService
+  ) {}
 
   ngOnInit() {
     // use service to get all recipes
@@ -36,11 +46,12 @@ export class InventoryCraftingComponent {
       this.recipes = data;
     });
 
-    this.resourceService.getAllResources(this.currentPlanet.name).subscribe((data) => {
-      this.inventory = data;
-      console.log(this.inventory);
-    });
-
+    this.resourceService
+      .getAllResources(this.currentPlanet.name)
+      .subscribe((data) => {
+        this.inventory = data;
+        console.log(this.inventory);
+      });
   }
 
   ngAfterViewInit() {
@@ -50,28 +61,58 @@ export class InventoryCraftingComponent {
   }
 
   handleClick(index: number) {
-
-    console.log(this.recipes[index]);
     this.selectedRecipe = this.recipes[index];
-    
-
     if (this.ingredientsElement) {
       this.ingredientsElement.nativeElement.style.height = '10px';
       this.yeildElement.nativeElement.style.height = '10px';
       setTimeout(() => {
         this.ingredientsElement.nativeElement.style.height = '41.5vh';
-        this.craftButtonElement.nativeElement.style.marginTop = '41.5vh'
+        this.craftButtonElement.nativeElement.style.marginTop = '41.5vh';
       }, 400);
       setTimeout(() => {
         this.yeildElement.nativeElement.style.height = '41.5vh';
       }, 600);
     }
+
+    console.log(this.isCraftable());
   }
 
+  // Sorry, This is a very ineffcient way to do this, I've done similar functionality somewhere else in this project without using this apporach
+  // I just really wanted to try this with an unnecessary usage of ifstatements to see if it would work
+  // I won't do this again, it isn't good to do it, but it was really fun to try
+  isCraftable() {
+    if (!this.selectedRecipe) {
+      return false;
+    }
+
+    if (this.ingredientsMet[0] === true) {
+      return true;
+    }
+
+    if (
+      this.selectedRecipe.ingredient_2_quantity === 0 ||
+      this.selectedRecipe.ingredient_2_quantity === null ||
+      this.selectedRecipe.ingredient_2_quantity === undefined
+    ) {
+      if (
+        this.ingredientsMet[0] === false &&
+        this.ingredientsMet[1] === true &&
+        this.ingredientsMet[2] === false
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      if (
+        this.ingredientsMet[0] === false &&
+        this.ingredientsMet[1] === true &&
+        this.ingredientsMet[2] === true
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 }
-
-
-
-
-
-
--- client/src/app/components/required-ingredient-card/required-ingredient-card.component.ts
+++ client/src/app/components/required-ingredient-card/required-ingredient-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-required-ingredient-card',
@@ -12,14 +12,27 @@ export class RequiredIngredientCardComponent {
   @Input() ingredientNumber: number = 0;
   @Input() inventory: any;
   @Input() requiredItemAmount: number = 0;
+  @Input() ingredientsMet: Array<boolean> = [false, false, false];
 
   constructor() {}
 
   ngOnInit(): void {
-    console.log(this.selectedRecipe);
     let number = this.ingredientNumber;
     let id = this.selectedRecipe['ingredient_' + number].id;
     let ingredient = this.inventory.find((item: any) => item.id === id);
     this.requiredItemAmount = ingredient.quantity;
+  }
+  ngOnChanges(): void {
+    let number = this.ingredientNumber;
+    let id = this.selectedRecipe['ingredient_' + number].id;
+    let ingredient = this.inventory.find((item: any) => item.id === id);
+    this.requiredItemAmount = ingredient.quantity;
+    if (
+      ingredient.quantity >
+      this.selectedRecipe['ingredient_' + this.ingredientNumber + '_quantity']
+    ) {
+      this.ingredientsMet[number] = true;
+      console.log(this.ingredientsMet);
+    }
   }
 }
